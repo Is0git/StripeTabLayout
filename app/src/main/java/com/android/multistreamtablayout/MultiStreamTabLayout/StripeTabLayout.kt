a59dd85cc2e1b29e8614b20a5b6a4327dc086a2d
Danielius <dnlspasin@gmail.com>
2020-02-02 12:47:06 +0200

    layoutParams fix and constraintSet

--- app/src/main/java/com/android/multistreamtablayout/MultiStreamTabLayout/StripeTabLayout.kt
+++ app/src/main/java/com/android/multistreamtablayout/MultiStreamTabLayout/StripeTabLayout.kt
@@ -3,65 +3,69 @@ package com.android.multistreamtablayout.MultiStreamTabLayout
 import android.content.Context
 import android.content.res.TypedArray
 import android.util.AttributeSet
-import android.view.animation.Animation
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.viewpager2.widget.ViewPager2
 import com.android.multistreamtablayout.R
-import com.google.android.material.tabs.TabItem
+
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
-class StripeTabLayout: ConstraintLayout {
+class StripeTabLayout : ConstraintLayout {
     lateinit var typedArray: TypedArray
     var showStripes: Boolean = true
+
     constructor(context: Context) : super(context) {
         init()
     }
 
-    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
-    {
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
         init()
     }
 
     lateinit var tabLayout: TabLayout
 
     fun init() {
-            typedArray = context.obtainStyledAttributes(R.styleable.StripeTabLayout).apply {
-                showStripes = getBoolean(R.styleable.StripeTabLayout_showStripes, true)
-                recycle()
-            }
+        typedArray = context.obtainStyledAttributes(R.styleable.StripeTabLayout).apply {
+            showStripes = getBoolean(R.styleable.StripeTabLayout_showStripes, true)
+            recycle()
+        }
 
 
-            tabLayout = TabLayout(context, null, R.style.tabCustomStyle).apply {
-                id = R.id.tabLayout
-                tabMode = TabLayout.MODE_FIXED
-                tabGravity = TabLayout.GRAVITY_FILL
-                this.isInlineLabel = true
-                this.isTabIndicatorFullWidth = true
-            }
+        tabLayout = TabLayout(context, null, R.style.tabCustomStyle).apply {
+            id = R.id.tabLayout
+            tabMode = TabLayout.MODE_FIXED
+            tabGravity = TabLayout.GRAVITY_FILL
+            this.layoutParams = ViewGroup.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+            this.isInlineLabel = true
+            this.isTabIndicatorFullWidth = true
+        }
 
-            tabLayout.addTab(tabLayout.newTab().setText("SDwqe"))
-            tabLayout.addTab(tabLayout.newTab().setText("SDwqe"))
+        tabLayout.addTab(tabLayout.newTab().setCustomView(R.layout.default_tab))
+        tabLayout.addTab(tabLayout.newTab().setCustomView(R.layout.default_tab))
 
+
+        addView(tabLayout)
         setConstraints()
-            addView(tabLayout)
 
 
     }
 
 
     fun setConstraints() {
-        val constraintSet = ConstraintSet().apply {
-            connect(tabLayout.id, ConstraintSet.BOTTOM, id, ConstraintSet.BOTTOM)
-            connect(tabLayout.id, ConstraintSet.END, id, ConstraintSet.END)
-        }
 
-        constraintSet.applyTo(this)
+        val constraintSet = ConstraintSet().apply {
+            clone(this@StripeTabLayout)
+            connect(tabLayout.id, ConstraintSet.BOTTOM, id, ConstraintSet.BOTTOM)
+        }
+        setConstraintSet(constraintSet)
     }
 
     fun setupWithViewPager(viewPager: ViewPager2) {
-        TabLayoutMediator(tabLayout, viewPager) {tab, position ->
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             tab.setCustomView(R.layout.default_tab)
         }
     }
